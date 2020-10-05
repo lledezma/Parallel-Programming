cdb6bda2d5bba1412ae60b29a01a67f3d9fff791
lledezma <luis.ledezma@pop.belmont.edu>
2020-10-04 20:37:54 -0500

    minor bug fixes

--- CUDA/add.cu
+++ CUDA/add.cu
@@ -1,5 +1,7 @@
+    //adding two values using CUDA
 #include <stdio.h>
 #include <stdlib.h>
+#include <cuda_runtime.h>
 __global__ void add(int *d_a, int *d_b, int *d_result) {
       *d_result = *d_a + *d_b;
 }
@@ -12,28 +14,28 @@ int main() {
   //declare device variables
   int *d_a;
   int *d_b;
-  int *result;
+  int *d_result;
 
   //Memory allocation of device variable
   cudaMalloc((void**)&d_a, sizeof(int));
   cudaMalloc((void**)&d_b, sizeof(int));
-  cudaMalloc((void**)&result, sizeof(int));
+  cudaMalloc((void**)&d_result, sizeof(int));
 
   //Copy Host memory to Device memory
   cudaMemcpy(d_a, &h_a, sizeof(int), cudaMemcpyHostToDevice);
   cudaMemcpy(d_b, &h_b, sizeof(int), cudaMemcpyHostToDevice);
 
   //Launch Kernel
-  add<<<1,1>>>(d_a,d_b,result);
+  add<<<1,1>>>(d_a,d_b,d_result);
 
   //copy device results to host results
-  cudaMemcpy(&h_result, result, sizeof(int), cudaMemcpyDeviceToHost);
+  cudaMemcpy(&h_result, d_result, sizeof(int), cudaMemcpyDeviceToHost);
 
   printf("the result is: %d\n",h_result);
 
   //free device memory
   cudaFree(d_a);
   cudaFree(d_b);
-  cudaFree(result);
+  cudaFree(d_result);
   return 0;
 }
