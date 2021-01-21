35354065152c2debeac77b7e7b9cf175a4b1ab93
lledezma <luis.ledezma@pop.belmont.edu>
2021-01-21 13:08:33 -0600

    added cuda pattern-match program

---
+++ CUDA/Pattern-Match/pattern.cu
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda_runtime.h>
+
+__global__ void add(char* pattern, char* string, int* results, int patLength, int strLength){
+	//get global thread id
+  int x = blockDim.x * blockIdx.x + threadIdx.x;
+
+  //check bounds
+  if (x <= strLength-patLength){                                                         
+    for(int i = 0; i < patLength; i++){                                               
+      if (pattern[i] != string[x+i])   //if a mismatch is found, exit.        
+        return;                                    
+    }                                                      
+		atomicAdd(results,1); //match has been found so we add 1 to results
+  }       
+}
+
+int main(){
+  //device variables
+  char* d_pattern;       //device pattern
+  char* d_string;        //device string
+  int* d_results;        //device variable to store results
+
+  //host variables
+  int h_results;        //host varible to store results  
+  const char* h_pattern = "NNTHVLTLP";
+  const char* h_string = "MIVNNTHVLTLPLYTTTTCHTHPHLYTNNTHVLTLPYSIYHLKLTLLSDSTSLHGPSCHTHNNTHVLTLPTHVLTLLTLLSDSTSRWGSK";
+  int h_patLength = (int)strlen(h_pattern); //length of pattern
+  int h_strLength = (int)strlen(h_string);  //length of string
+
+  //Memory allocation of device variables
+  cudaMalloc((void**)&d_pattern, h_patLength*sizeof(char));
+  cudaMalloc((void**)&d_string, h_strLength*sizeof(char));
+  cudaMalloc((void**)&d_results, sizeof(int));
+
+  //Copy Host memory to Device Memory
+  cudaMemcpy(d_pattern,h_pattern, h_patLength*sizeof(char), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_string,h_string, h_strLength*sizeof(char), cudaMemcpyHostToDevice);
+
+  //Launch Kernel
+  add<<<1,h_strLength>>>(d_pattern,d_string,d_results,h_patLength, h_strLength);      //A grid with one block and strLength threads.
+
+  //copy device results to host results
+  cudaMemcpy(&h_results, d_results, sizeof(int), cudaMemcpyDeviceToHost);
+
+  //print results 
+  printf("Total number of matches: %d\n", h_results);
+
+  //free device memory
+  cudaFree(d_pattern);
+  cudaFree(d_string);
+  cudaFree(d_results);
+  
+	return 0;
+}
