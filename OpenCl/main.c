196caede854d61c75036bfa7bf4d665c02e8f712
lledezma <luis.ledezma@pop.belmont.edu>
2021-01-28 22:01:36 -0600

    new program to use all devices available

--- OpenCl/main.c
+++ OpenCl/main.c
@@ -1,11 +1,9 @@
-                //Array addition using OpenCL
+                //Addind two arrays using all available devices and comparing times
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <OpenCL/opencl.h>
 #include <stdio.h>
-
-#define USE_CPU
 
 // The kernel
 const char *KernelSource =                                     "\n" \
@@ -25,21 +23,22 @@ const char *KernelSource =                                     "\n" \
 "    }                                                          \n" \
 "}                                                              \n" ;
 
-int maxCompUnits(); //get the max compute units available
-const char* deviceName(); //get the name of device
+int maxCpuUnits(cl_uint num_platforms, cl_uint platformID, cl_uint num_devices, cl_uint deviceID); //get the max compute units available
+const char* deviceName(cl_uint num_platforms, cl_uint platformID, cl_uint num_devices, cl_uint deviceID); //get the name of device
 
 int main(int argc, const char * argv[]) {
-    int err;                    //varible to track errors
-    cl_platform_id platform;
-    cl_device_id device_id;
+    int err; //varible to track errors
+    cl_platform_id* platform;
+    cl_device_id* device_id;
     cl_context context;
     cl_command_queue queue;
     cl_program program;
     cl_kernel kernel;
+    cl_event event;
     size_t globalWorkSize;  //global work items
     size_t localWorkSize;   //work items per group
 
-    const int num = 100;     //size of arrays
+    const int num = 100000000;     //size of arrays
     
     //declaring device variables
     cl_mem d_a;
@@ -51,121 +50,139 @@ int main(int argc, const char * argv[]) {
     int* h_b;
     int* h_c;
     
-    //allocating host memory
+    //initializing host memory
     int bytes = num*sizeof(int);
     h_a = (int*)malloc(bytes);
     h_b = (int*)malloc(bytes);
     h_c = (int*)malloc(bytes);
     
-    //initializing host variables 
     for(int i = 0; i < num; i++){
         h_a[i] = i+1;
         h_b[i] = i+1;
     }
-    // # of platform IDs || platform || # of OpenCL platforms available
-    err = clGetPlatformIDs(1, &platform, NULL);
+    
+    cl_uint num_platforms; //number of platforms
+    err = clGetPlatformIDs(0, NULL, &num_platforms); //get the number of platforms found
     if(err != CL_SUCCESS){
-        printf("Error getting the platform\n");
+        printf("Error getting the number of platforms\n");
+        return EXIT_FAILURE;
+    }
+    platform = (cl_platform_id*)malloc(sizeof(cl_platform_id)*num_platforms); //allocate memory for platform IDs
+    err = clGetPlatformIDs(num_platforms, platform, NULL); //get the IDs of available platforms
+    if(err != CL_SUCCESS){
+        printf("Error getting the platforms\n");
         return EXIT_FAILURE;
     }
     
-    //get device ID
-    #ifdef USE_CPU
-        err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_CPU, 1, &device_id, NULL);
-    #else
-        err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device_id, NULL);
-    #endif
-    if(err != CL_SUCCESS){
-        printf("Error getting the device IDs\n");
-        return EXIT_FAILURE;
+    for(cl_uint h = 0; h < num_platforms; h++)
+    {
+
+        cl_uint num_devices; // to hold the number of devices found
+        err = clGetDeviceIDs(platform[h], CL_DEVICE_TYPE_ALL, 0, NULL, &num_devices); //get number of devices found
+
+        device_id = calloc(sizeof(cl_device_id), num_devices); //allocate memory for device IDs
+        clGetDeviceIDs(platform[h], CL_DEVICE_TYPE_ALL, num_devices, device_id, NULL); //get ids of all devices available
+
+        //create context
+        context = clCreateContext(0, num_devices, device_id, NULL, NULL, &err);
+        if(err != CL_SUCCESS){
+            printf("Could not create context\n");
+            return EXIT_FAILURE;
+        }
+
+        for(cl_uint j = 0; j < num_devices; j++)
+        {
+            // printf("%d\n", j);
+            //create command queue
+            queue = clCreateCommandQueue(context, device_id[j], CL_QUEUE_PROFILING_ENABLE, &err);
+            if(err != CL_SUCCESS){
+                printf("Error creating the command queue\n");
+                return EXIT_FAILURE;
+            }
+            
+            program = clCreateProgramWithSource(context, 1, (const char **)&KernelSource, NULL, &err);
+            if(err != CL_SUCCESS){
+                printf("Error creating the program\n");
+                return EXIT_FAILURE;
+            }
+
+            // built the program
+            err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+            if(err != CL_SUCCESS){
+                printf("Error building the program\n");
+                return EXIT_FAILURE;
+            }
+            
+            kernel = clCreateKernel(program, "addArray", &err);
+            if(err != CL_SUCCESS){
+                printf("error creating the kernel\n");
+                return EXIT_FAILURE;
+            }
+            
+            //allocate device memory
+            d_a = clCreateBuffer(context, CL_MEM_READ_ONLY, bytes, NULL, &err);
+            d_b = clCreateBuffer(context, CL_MEM_READ_ONLY, bytes, NULL, &err);
+            d_c = clCreateBuffer(context, CL_MEM_READ_WRITE, bytes, NULL, &err);
+            if(err != CL_SUCCESS){
+                printf("Error allocating device memory\n");
+                return EXIT_FAILURE;
+            }
+            
+            //copy data from host variables to device variables
+            err = clEnqueueWriteBuffer(queue, d_a, CL_TRUE, 0, bytes, h_a, 0, NULL, NULL);
+            err |= clEnqueueWriteBuffer(queue, d_b, CL_TRUE, 0, bytes, h_b, 0, NULL, NULL);
+            if(err != CL_SUCCESS){
+                printf("Error copying data to device variables\n");
+                return EXIT_FAILURE;
+            }
+            
+            // Set the Kernel Arguments
+            err = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void*)&d_a);
+            err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), (void*)&d_b);
+            err |= clSetKernelArg(kernel, 2, sizeof(cl_mem), (void*)&d_c);
+            err |= clSetKernelArg(kernel, 3, sizeof(int), (void*)&num);
+            if(err != CL_SUCCESS){
+                printf("Error setting the kernel arguments\n");
+                return EXIT_FAILURE;
+            }
+            
+            globalWorkSize = maxCpuUnits(num_platforms, h, num_devices, j);   //number of global work items
+            localWorkSize = 2;                //number of work items per group
+            
+            //Execute the kernel
+            err = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &globalWorkSize, &localWorkSize, 0, NULL, &event);
+            if(err != CL_SUCCESS){
+                printf("Error executing the Kernel");
+                return EXIT_FAILURE;
+            }
+
+            //wait for all commands in the queue to be processed and completed
+            clWaitForEvents(1, &event);
+            clFinish(queue);
+            
+            //copy device memory to host memory
+            err = clEnqueueReadBuffer(queue, d_c, CL_TRUE, 0, bytes, h_c, 0, NULL, NULL);
+            if(err != CL_SUCCESS){
+                printf("Error copying device memory to host memory\n");
+                return EXIT_FAILURE;
+            }
+            
+        ///*   print device info, compute units, and results
+            printf("Running on device: %s with %d compute units.\n", deviceName(num_platforms, h, num_devices, j), maxCpuUnits(num_platforms, h, num_devices, j));
+            // for(int i = 0; i < num; i++){
+            //     printf("%d   =   %d  +   %d\n", h_c[i], h_a[i], h_b[i]);
+            // }
+            cl_ulong time_start;
+            cl_ulong time_end;
+            clGetEventProfilingInfo(event, CL_PROFILING_COMMAND_START, sizeof(time_start), &time_start, NULL);
+            clGetEventProfilingInfo(event, CL_PROFILING_COMMAND_END, sizeof(time_end), &time_end, NULL);
+            double nanoSeconds = time_end-time_start;
+            printf("OpenCl Execution time is: %0.3f milliseconds \n",nanoSeconds / 1000000.0);
+        //*/
+        }
     }
-    
-    //create context
-    context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
-    if(err != CL_SUCCESS){
-        printf("Could not create context\n");
-        return EXIT_FAILURE;
-    }
-    
-    //create command queue
-    queue = clCreateCommandQueue(context, device_id, 0, &err);
-    if(err != CL_SUCCESS){
-        printf("Error creating the command queue\n");
-        return EXIT_FAILURE;
-    }
-    
-    program = clCreateProgramWithSource(context, 1, (const char **)&KernelSource, NULL, &err);
-    if(err != CL_SUCCESS){
-        printf("Error creating the program\n");
-        return EXIT_FAILURE;
-    }
-    // built the program
-    err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
-    if(err != CL_SUCCESS){
-        printf("Error building the program\n");
-        return EXIT_FAILURE;
-    }
-    
-    kernel = clCreateKernel(program, "addArray", &err);
-    if(err != CL_SUCCESS){
-        printf("error creating the kernel\n");
-        return EXIT_FAILURE;
-    }
-    
-    //allocate device memory
-    d_a = clCreateBuffer(context, CL_MEM_READ_ONLY, bytes, NULL, &err);
-    d_b = clCreateBuffer(context, CL_MEM_READ_ONLY, bytes, NULL, &err);
-    d_c = clCreateBuffer(context, CL_MEM_READ_WRITE, bytes, NULL, &err);
-    if(err != CL_SUCCESS){
-        printf("Error allocating device memory\n");
-        return EXIT_FAILURE;
-    }
-    
-    //copy data from host variables to device variables
-    err = clEnqueueWriteBuffer(queue, d_a, CL_TRUE, 0, bytes, h_a, 0, NULL, NULL);
-    err |= clEnqueueWriteBuffer(queue, d_b, CL_TRUE, 0, bytes, h_b, 0, NULL, NULL);
-    if(err != CL_SUCCESS){
-        printf("Error copying data to device variables\n");
-        return EXIT_FAILURE;
-    }
-    
-    // Set the Kernel Arguments
-    err = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void*)&d_a);
-    err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), (void*)&d_b);
-    err |= clSetKernelArg(kernel, 2, sizeof(cl_mem), (void*)&d_c);
-    err |= clSetKernelArg(kernel, 3, sizeof(int), (void*)&num);
-    if(err != CL_SUCCESS){
-        printf("Error setting the kernel arguments\n");
-        return EXIT_FAILURE;
-    }
-    
-    globalWorkSize = maxCompUnits();      //number of global work items
-    localWorkSize = 2;          	 //number of work items per group
-    
-    //Execute the kernel
-    err = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &globalWorkSize, &localWorkSize, 0, NULL, NULL);
-    if(err != CL_SUCCESS){
-        printf("Error executing the Kernel");
-        return EXIT_FAILURE;
-    }
-    
-    //wait for all commands in the queue to be processed and completed
-    clFinish(queue);
-    
-    //copy device memory to host memory
-    err = clEnqueueReadBuffer(queue, d_c, CL_TRUE, 0, bytes, h_c, 0, NULL, NULL);
-    if(err != CL_SUCCESS){
-        printf("Error copying device memory to host memory\n");
-        return EXIT_FAILURE;
-    }
-    
-///*   print device info, compute units, and results
-    printf("Running on device: %s with %d compute units.\n", deviceName(), maxCompUnits());
-    for(int i = 0; i < num; i++){
-        printf("%d   =   %d  +   %d\n", h_c[i], h_a[i], h_b[i]);
-    }
-//*/
     //release device memory
+    clReleaseEvent(event);
     clReleaseMemObject(d_a);
     clReleaseMemObject(d_b);
     clReleaseMemObject(d_c);
@@ -182,36 +199,42 @@ int main(int argc, const char * argv[]) {
     return 0;
 }
 
-int maxCompUnits(){
+int maxCpuUnits(cl_uint num_platforms, cl_uint platformID, cl_uint num_devices, cl_uint deviceID){
     int err;
-    cl_device_id device_id;
-    cl_uint maxComputeUnits;
-    #ifdef USE_CPU
-        err = clGetDeviceIDs(NULL, CL_DEVICE_TYPE_CPU, 1, &device_id, NULL);
-    #else
-        err = clGetDeviceIDs(NULL, CL_DEVICE_TYPE_GPU, 1, &device_id, NULL);
-    #endif
+    cl_platform_id* platform = (cl_platform_id*)malloc(sizeof(cl_platform_id)*num_platforms); //allocate memory for platform IDs
+    err = clGetPlatformIDs(num_platforms, platform, NULL); //get the IDs of available platforms
     if(err != CL_SUCCESS){
-        printf("Error getting device id from maxCompUnits function\n");
+        printf("Error getting the platforms\n");
+        return EXIT_FAILURE;
+    }
+
+    cl_device_id* devices = calloc(sizeof(cl_device_id), num_devices);
+    cl_uint maxComputeUnits;
+    err = clGetDeviceIDs(platform[platformID], CL_DEVICE_TYPE_ALL, num_devices, devices, NULL);
+    if(err != CL_SUCCESS){
+        printf("Error getting device id from maxCpuUnits function\n");
         return EXIT_FAILURE;
     }
     
-    err = clGetDeviceInfo(device_id, CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(maxComputeUnits), &maxComputeUnits, NULL);
+    err = clGetDeviceInfo(devices[deviceID], CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(maxComputeUnits), &maxComputeUnits, NULL);
     if(err != CL_SUCCESS){
-        printf("Error getting device info from maxCompUnits function\n");
+        printf("Error getting device info from maxCpuUnits function\n");
         return EXIT_FAILURE;
     }
     return maxComputeUnits;
 }
 
-const char* deviceName(){
+const char* deviceName(cl_uint num_platforms, cl_uint platformID, cl_uint num_devices, cl_uint deviceID){
     int err;
-    cl_device_id device_id;
-    #ifdef USE_CPU
-        err = clGetDeviceIDs(NULL, CL_DEVICE_TYPE_CPU, 1, &device_id, NULL);
-    #else
-        err = clGetDeviceIDs(NULL, CL_DEVICE_TYPE_GPU, 1, &device_id, NULL);
-    #endif
+    cl_platform_id* platform = (cl_platform_id*)malloc(sizeof(cl_platform_id)*num_platforms); //allocate memory for platform IDs
+    err = clGetPlatformIDs(num_platforms, platform, NULL); //get the IDs of available platforms
+    if(err != CL_SUCCESS){
+        printf("Error getting the platforms\n");
+        exit(EXIT_FAILURE);
+    }
+
+    cl_device_id* devices = calloc(sizeof(cl_device_id), num_devices);
+    err = clGetDeviceIDs(platform[platformID], CL_DEVICE_TYPE_ALL, num_devices, devices, NULL);
     if(err != CL_SUCCESS){
         printf("Error getting device id from deviceName function\n");
         exit(EXIT_FAILURE);
@@ -219,7 +242,7 @@ const char* deviceName(){
 
     size_t valueSize;
     char* nameOfDevice = (char*)malloc(sizeof(valueSize));
-    err = clGetDeviceInfo(device_id, CL_DEVICE_NAME, valueSize, nameOfDevice, NULL);
+    err = clGetDeviceInfo(devices[deviceID], CL_DEVICE_NAME, valueSize, nameOfDevice, NULL);
     if(err != CL_SUCCESS){
         printf("Error getting device name from deviceName function\n");
         exit(EXIT_FAILURE);
